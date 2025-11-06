95fecec6ba0f95142330749d8f40b9c2c097884e
Kevin Guzmán <knngzmn@gmail.com>
2025-11-05 21:07:24 -0500

    Prueba Error

--- testTunit/Tests.cs
+++ testTunit/Tests.cs
@@ -6,7 +6,7 @@ using BlazorApp1.Components.Pages;
 
 public class Tests
 {
-   /* [Test]
+    [Test]
     public async Task Basic()
     {
         Console.WriteLine("This is a Basic Test");
@@ -14,8 +14,8 @@ public class Tests
         var Componente = Bunitctx.RenderComponent<Counter>();
         Componente.Find("button").Click();
         var Resultado = Componente.Find("p[role='status']");
-        await TUnit.Assertions.Assert.That(Resultado.TextContent).Contains("Current count: 1");
-    }*/
+        await TUnit.Assertions.Assert.That(Resultado.TextContent).Contains("Current count: 2");
+    }
 
     [Test]
 
